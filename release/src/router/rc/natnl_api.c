20f63f339da9a3c7116e6ea6c1a13db402122b71
Your Name <you@example.com>
2022-03-13 16:14:03 +0100

    Initial import from 3.0.0.4.380.10931, exclude not needed packages

---
+++ release/src/router/rc/natnl_api.c
@@ -0,0 +1,60 @@
+
+#include "rc.h"
+#ifdef RTCONFIG_TUNNEL
+void start_aae()
+{
+	if( getpid()!=1 ) {
+		notify_rc("start_aae");
+		return;
+	}
+
+	if ( !pids("aaews" )){
+		//nvram_set("aae_enable", "1");
+		system("aaews &");
+		logmessage("AAE", "AAE Service is started");
+	}
+}
+
+void stop_aae()
+{
+	if( getpid()!=1 ) {
+		notify_rc("stop_aae");
+		return;
+	}
+	
+	// remove enable
+	nvram_set_int("aae_enable", (nvram_get_int("aae_enable") & ~1));
+	killall_tk("aaews");
+	logmessage("NAT Tunnel", "AAE Service is stopped");
+}
+
+void start_mastiff()
+{
+	stop_aae();
+	
+	if( getpid()!=1 ) {
+		notify_rc("start_mastiff");
+		return;
+	}
+
+	if ( !pids("mastiff" )){
+		system("mastiff &");
+		logmessage("AAE", "AAE Service is started");
+		//start_aae();
+	}
+
+}
+
+void stop_mastiff()
+{
+	if( getpid()!=1 ) {
+		notify_rc("stop_mastiff");
+		return;
+	}
+	
+	killall_tk("mastiff");
+	logmessage("NAT Tunnel", "AAE Service is stopped");
+	
+	stop_aae();
+}
+#endif
